784a9cef1ea1a02ed6c9f667372d3b1e3eb2ff5e
Saad24 <saad.24.ahm@gmail.com>
2023-01-30 17:22:44 +0000

    Tidied app.js and added navbar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
 import './assets/style.css'; 
+import NavBar from './components/NavBar';
 import ConcertContainer from './containers/ConcertContainer';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-  <>
-<div className = "header">
-<style className ="font">
-@import url('https://fonts.googleapis.com/css2?family=Dosis&display=swap');
-</style>
-  <img className = "SoundStageLogo" src= {require("./assets/soundstage.png")}></img>
-    {/* <h2>SoundStage</h2> */}
-<nav className= "nav"> 
-  <a href="#">About</a>
-  <a href="#">Events</a>
-  <a href="#">Pricing</a>
-  <a href="#"></a>
- 
-  </nav>
- </div>
- <ConcertContainer/></>
+  <BrowserRouter>
+    <div className="App">
+
+    <NavBar/>
+    <ConcertContainer/>
+    </div>
+
+    <Routes>
+      
+
+
+    </Routes>
+
+    </BrowserRouter>
+
 );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import '../assets/style.css'; 
+
+const NavBar = () => {
+    return ( 
+    <div className = "header">
+    <style className ="font">
+    @import url('https://fonts.googleapis.com/css2?family=Dosis&display=swap');
+    </style>
+      <img className = "SoundStageLogo" src= {require("../assets/soundstage.png")}></img>
+        {/* <h2>SoundStage</h2> */}
+    <nav className= "nav"> 
+      <a href="#">About</a>
+      <a href="#">Events</a>
+      <a href="#">Pricing</a>
+      <a href="#"></a>
+     
+      </nav>
+     </div>
+    )}
+
+    export default NavBar;
+     
