5a4c7e1fdc19cf8e719249b9bb9e46019a20b00c
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-01-31 20:14:20 +0000

    changing style.css with header and nav alterations ebm

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,38 @@
-import { Link } from 'react-router-dom';
-import '../assets/style.css'; 
+import { Link } from "react-router-dom";
+import "../assets/style.css";
 
 const NavBar = () => {
-    return ( 
-    <div className = "header">
-    <style className ="font">
-    @import url('https://fonts.googleapis.com/css2?family=Dosis&display=swap');
-    </style>
-      <img className = "SoundStageLogo" src= {require("../assets/soundstage.png")}></img>
-        {/* <h2>SoundStage</h2> */}
+  return (
+    <div className="header">
+      <style className="font">
+        @import
+        url('https://fonts.googleapis.com/css2?family=Dosis&display=swap');
+      </style>
+      <img
+        className="SoundStageLogo"
+        src={require("../assets/SoundStageLogo.png")}
+      ></img>
+      {/* <h2>SoundStage</h2> */}
 
-
-    <nav className= "nav">
-      <button><Link to="/">HOME</Link></button>
-      <button> <a href="#">ABOUT</a></button>
-      <button><Link to="/concerts">CONCERTS</Link></button>
-      <button><a href="#">PRICING</a></button>
-      {/* <a href="#"></a> */}
-     
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            {" "}
+            <a href="#">ABOUT</a>
+          </li>
+          <li>
+            <Link to="/concerts">CONCERTS</Link>
+          </li>
+          <li>
+            <a href="#">PRICING</a>
+          </li>
+        </ul>
       </nav>
+    </div>
+  );
+};
 
-      
-
-     </div>
-    )}
-
-    export default NavBar;
-     
+export default NavBar;
