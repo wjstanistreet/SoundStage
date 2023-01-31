bd673a930014162666c2e9a31cf4c1eeda5f2625
Saad24 <saad.24.ahm@gmail.com>
2023-01-31 12:28:12 +0000

    added drop down for sort functions

--- src/containers/ConcertContainer.js
+++ src/containers/ConcertContainer.js
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import ConcertCard from "../components/ConcertCard";
 import ConcertList from "../components/ConcertList";
 import SearchBar from "../components/SearchBar";
+import Concerts from "../pages/Concerts";
 
-const ConcertContainer = () => {
+const ConcertContainer = ({sortedConcerts}) => {
     
     const [concerts, setConcerts] = useState([]);
     const [searchInput, setSearchInput] = useState("");
@@ -14,10 +15,14 @@ const ConcertContainer = () => {
         .then((data) => {setConcerts(data)});
     }, [])
 
+        
+        console.log(sortedConcerts)
+
     return ( 
         <>
             <SearchBar setSearchInput={setSearchInput}/>
             <ConcertList concerts={concerts} searchInput={searchInput}/>
+
         </>
     );
 }
--- src/pages/Concerts.js
+++ src/pages/Concerts.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
+
 import ConcertContainer from "../containers/ConcertContainer";
 
 const Concerts = () => {
-    return (  
-        <ConcertContainer></ConcertContainer>
+    
+    const [sortedConcerts, setSortedConcerts] = useState([]);
+
+    const handleSort = (event) => {
+        fetch(`http://localhost:8080/concerts/${event.target.value}`)
+        .then((response) => {return response.json()})
+        .then((data) => {setSortedConcerts(data)});
+    }
+        console.log(sortedConcerts)
+    
+    
+    return (
+        <div>
+            <select onChange={handleSort}>
+                <option disabled-value=""> -- Sort by -- </option>
+                <option value="ticketPrices">Descending Ticket Price</option>
 
 
+                <option value="capacities">Ascending Capacities</option>
+            </select>
+
+            <ConcertContainer></ConcertContainer>
+        </div>
     );
 }
  
