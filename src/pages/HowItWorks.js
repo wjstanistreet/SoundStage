75fdfad6b71b0a00fdb22f1103c5cfed2091d057
Emma Bayley-Melendez <e.melendez98@outlook.com>
2023-01-31 22:59:57 +0000

    styling done with HowItWorks.js file in style.css

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -6,6 +6,7 @@ const HowItWorks = () => {
             <p>Events made easy!</p>
             <h1>How it works</h1>
             <p>SoundStage makes it simple for you to attend and create events. </p>
+            <img className="planningImage" src={require("../assets/Planning.png")}></img>
         </div>
 
      );
