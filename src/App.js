e66a0ca6323dddf11caaf34aff671ad289345c7f
Saad24 <saad.24.ahm@gmail.com>
2023-01-31 11:35:03 +0000

    Added concerts page and home page

--- src/App.js
+++ src/App.js
@@ -5,25 +5,21 @@ import ConcertContainer from './containers/ConcertContainer';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Footer from './components/Footer';
 import Concerts from './pages/Concerts';
+import Home from './pages/Home';
 
 
 function App() {
   return (
   <BrowserRouter>
     <div className="App">
-
     <NavBar/>
-    <img className ="concertImage" src= {require("./assets/concert.jpg")}></img> 
-    <ConcertContainer/>
-    
-
-   
-    </div>
-
     <Routes>
+      <Route path="/" element={<Home />}/>
       <Route path="/concerts" element={<Concerts/>}/>
     </Routes>
     <Footer/>
+    </div>
+
     </BrowserRouter>
 
 );
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import ConcertContainer from "../containers/ConcertContainer";
+
+const  Home = () => {
+    return (
+    <div>
+    <img className ="concertImage" src= {require("../assets/concert.jpg")}></img> 
+    <ConcertContainer/>
+    </div>
+
+    );
+}
+ 
+export default Home;
