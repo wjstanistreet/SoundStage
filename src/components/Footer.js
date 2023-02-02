0b5678f55d84e4dab5d5619deb8aa0fa808f6e98
Saad24 <saad.24.ahm@gmail.com>
2023-02-02 14:11:48 +0000

    fixed all formatting issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,16 @@
-
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-         <footer className="footer">
+      <footer className="footer">
         <h3><b>Company</b></h3>
         <h3><b>Support</b></h3>     
            <a href="#">Contact us!</a>
         <h3><b>Services</b></h3>
         <h3><b>What's On</b></h3>
-        <a href="#">Events</a>
+        <Link to="/concerts">Concerts</Link>
         <h2>Getting you straight to the stage...</h2>
+        <p>SoundStage 2023 ©</p>
 
         </footer>
 
